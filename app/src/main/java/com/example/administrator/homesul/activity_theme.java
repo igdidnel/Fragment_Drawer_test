4649768422abc32edba3303e40f7693c53ca1176
ChoiTeaHo <layup3@naver.com>
2017-07-02 14:43:12 +0900

    Initial commit

---
+++ app/src/main/java/com/example/administrator/homesul/activity_theme.java
@@ -0,0 +1,23 @@
+package com.example.administrator.homesul;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2017-07-01.
+ */
+
+public class activity_theme extends Fragment{
+    View v;  //뷰 객체를 하나 만들었음.
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
+       v = inflater.inflate(R.layout.activity_theme, container, false);
+        return v; //v값 반환
+    }
+}
